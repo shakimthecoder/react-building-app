7533f31c151476afd28896c50b81720a18ee10de
amenraprod <amenraprod@gmail.com>
2019-05-26 21:36:00 -0400

    added homepage template jsx

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'React';
+import 'react-dom' from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return(
+        <div>Homepage update</div>
+        );
+    }
+    
+}
+
+React.DOM
