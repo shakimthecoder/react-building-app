1808c1fd41c82360b0331e1c4d2eca0aaa94ed3e
amenraprod <amenraprod@gmail.com>
2019-05-26 21:52:47 -0400

    removed duplicate class case

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from 'React';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-class Homepage extends React.Component {
-    render(){
-        return(
-        <div>Homepage update</div>
-        );
-    }
-    
-}
 
 ReactDOM.render(<App />,
     document.querySelector('#root'));
