433a58a82d30466edc18ed0a039ff448ece4fe34
amenraprod <amenraprod@gmail.com>
2019-05-28 19:22:57 -0400

    properly closed input tag

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,16 +4,16 @@ class SearchBar extends React.Component {
 
     state = { term : '' };
 
-    
+
     render()
     {
         return(
             <div className="ui field">
                 <form className="search-style">
-                    <div className="">
-                        <input form="search-bar">
+                    <div className="search-section">
+                        <input form="search-bar" />
                             <label>Search</label>
-                          </input>
+
                       </div>
                      </form>
                 </div>
