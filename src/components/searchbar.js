cca569611535a1e9d4e734930b0f0115c864c518
amenraprod <amenraprod@gmail.com>
2019-05-28 17:18:28 -0400

    added  form input

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,6 +4,15 @@ class SearchBar extends React.Component {
     render()
     {
         return(
+            <div className="ui field">
+                <form className="search-style">
+                    <div className="">
+                        <input form="search-bar">
+                            <label>Search</label>
+                          </input>
+                      </div>
+                     </form>
+                </div>
 
         );
     }
