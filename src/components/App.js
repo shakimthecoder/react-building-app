67f785bd09c3057f3617c70d7496c3c66b8d16fc
amenraprod <amenraprod@gmail.com>
2019-05-28 17:02:51 -0400

    added div to app state

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 class App extends React.Component {
     render(){
-        return(
-        <div>Here</div>
-        );
+        return <div>Here</div>;
+        
     }
 }
 
