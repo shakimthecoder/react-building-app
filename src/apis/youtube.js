05ed279b4008d186f84f5802dac6584cb67a7c7c
amenraprod <amenraprod@gmail.com>
2019-05-30 17:15:50 -0400

    created axios function for api request

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
+
 const KEY = 'AIzaSyAFVcVH23pU-dHA2zt2QFcOy7uhHpWy-y4';
+
+export default axios.create({
+   baseURL: 'https://googleapis.com/youtube/v3',
+   params: {
+       part: 'snippet' 
+       key:
+       q:
+       maxResults: 5
+   }
+});
+
+
+
