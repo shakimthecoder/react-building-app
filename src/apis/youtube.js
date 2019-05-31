5d5a8673cfd8ebd6ed3d6b83fe75ef30d4671202
amenraprod <amenraprod@gmail.com>
2019-05-30 20:26:46 -0400

    fixed syntax

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,7 +6,7 @@ const KEY = 'AIzaSyAFVcVH23pU-dHA2zt2QFcOy7uhHpWy-y4';
 export default axios.create({
    baseURL: 'https://googleapis.com/youtube/v3',
    params: {
-       part: 'snippet' 
+       part: 'snippet' ,
        key:
        q:
        maxResults: 5
